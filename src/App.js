4dfa970b723aaed2c0734bf65597b9ed33e694a5
Javier Ortiz <javier.ortiz13@upr.edu>
2020-06-18 18:27:20 -0400

    styling for navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import styled from 'styled-components'
 import { NavigationBar } from './NavigationBar'
 import './App.css';
+
+const Styles = styled.div`{
+  background-color: hsla(227, 74%, 35%, 1);
+  min-height: 100vh;
+  color: hsla(35, 18%, 96%, 1);
+}
+`;
 
 function App() {
   return (
     <React.Fragment>
-      <NavigationBar />
-      // Home
-      <p>elo wol</p>
+      <Styles>
+        <NavigationBar />
+        // Home
+        <p>elo wol</p>
+      </Styles>
     </React.Fragment>
   );
 }
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -11,6 +11,9 @@ const Styles = styled.div`
 
 .champagne{
   color: hsla(35, 18%, 96%, 1);
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
 }
 }
 `;
@@ -19,14 +22,16 @@ export function NavigationBar(){
   return (
     <Styles>
       <Navbar bg="royal">
-        <Navbar.Brand href="#home" className="champagne">
+        <Navbar.Brand href="#home">
           <img
             alt=""
             src={icon}
-            width="30"
-            height="30"
+            width="60"
+            height="60"
             className="d-inline-block align-top"
             />{' '}
+          </Navbar.Brand>
+          <Navbar.Brand className="champagne">
             Ocean Typing
           </Navbar.Brand>
         </Navbar>
