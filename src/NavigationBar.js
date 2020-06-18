9e9d67135034234dea98ff31e7efd010e7d8996a
Javier Ortiz <javier.ortiz13@upr.edu>
2020-06-18 18:15:51 -0400

    added navbar

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -2,26 +2,34 @@ import React from 'react'
 import { Navbar } from 'react-bootstrap'
 import styled from 'styled-components'
 
-const Styles = styled.div`
+import icon from './temp_logo.png';
 
+const Styles = styled.div`
+.bg-royal{
+  background-color: hsla(227, 74%, 35%, 1);
+}
+
+.champagne{
+  color: hsla(35, 18%, 96%, 1);
+}
 }
 `;
 
 export function NavigationBar(){
   return (
     <Styles>
-      <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">
-      <img
-        alt=""
-        src="/logo.svg"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-      />{' '}
-      React Bootstrap
-    </Navbar.Brand>
-  </Navbar>
-    </Styles>
-  );
-}
+      <Navbar bg="royal">
+        <Navbar.Brand href="#home" className="champagne">
+          <img
+            alt=""
+            src={icon}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            />{' '}
+            Ocean Typing
+          </Navbar.Brand>
+        </Navbar>
+      </Styles>
+    );
+  }
