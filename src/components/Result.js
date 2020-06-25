2c52875a320e7b55f909c6a88dfeb13587e37838
Javier Ortiz <javier.ortiz13@upr.edu>
2020-06-25 17:06:41 -0400

    styled modal

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,23 +1,6 @@
 import React, { useState } from 'react';
 import { ListGroup, Modal } from 'react-bootstrap';
-import styled from 'styled-components'
-
-const Styles = styled.div`
-.bg-royal{
-  background-color: hsla(227, 74%, 35%, 1);
-}
-
-.title{
-  align-items: center;
-  justify-content: center;
-  font-size: calc(12px + 2vmin);
-}
-
-.champagne{
-  color: hsla(35, 18%, 96%, 1);
-}
-}
-`;
+import './Result.css'
 
 export function Result(props){
   const [show, setShow] = useState(true);
@@ -33,26 +16,24 @@ export function Result(props){
 
   return (
     <React.Fragment>
-      <Styles>
       <Modal
-        dialogClassName="side-modal"
         show={show}
         onHide={handleClose}
+        dialogClassName="bg-indigo result-modal"
         >
         <Modal.Header closeButton>
           <Modal.Title>Results: </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <ListGroup>
+          <ListGroup variant="flush" className="bg-indigo">
             {props.items.map(({label, name, data}) =>(
-              <ListGroup.Item action variant="primary" key={name}>
+              <ListGroup.Item className="list-indigo" key={name}>
                 {label}: {data}
               </ListGroup.Item>
             ))}
           </ListGroup>
         </Modal.Body>
       </Modal>
-      </Styles>
     </React.Fragment>
   );
 }
