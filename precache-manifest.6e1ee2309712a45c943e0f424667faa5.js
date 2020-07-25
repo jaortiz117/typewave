41308173006f87d93a3b606f489442d6b849a657
Javier Ortiz <javier.ortiz13@upr.edu>
2020-07-25 18:25:07 -0400

    Updates

---
+++ precache-manifest.6e1ee2309712a45c943e0f424667faa5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb523037f2b3c6b75c3f3cb831644fb6",
+    "url": "/typewave/index.html"
+  },
+  {
+    "revision": "f954c3709e4217ec0773",
+    "url": "/typewave/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "560026f805118fad4147",
+    "url": "/typewave/static/css/main.ded5d3c8.chunk.css"
+  },
+  {
+    "revision": "f954c3709e4217ec0773",
+    "url": "/typewave/static/js/2.d3e939ae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/typewave/static/js/2.d3e939ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "560026f805118fad4147",
+    "url": "/typewave/static/js/main.b139e3b8.chunk.js"
+  },
+  {
+    "revision": "26a91ef71cb47a12ab16",
+    "url": "/typewave/static/js/runtime-main.d541b057.js"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/typewave/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/typewave/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2001f2e68d1f3e3d0842b4d05587b5fc",
+    "url": "/typewave/static/media/tw-logo.2001f2e6.png"
+  }
+]);
