6832ddb87f4c3ed7737d3610d839fcc00d27a43c
Javier Ortiz <javier.ortiz13@upr.edu>
2020-07-25 17:32:04 -0400

    changed global font

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import WebFont from 'webfontloader';
+
+import './index.css';
+
+WebFont.load({
+  google: {
+    families: ['Titillium Web', 'sans-serif']
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
